32d5b8743f67cc69f59a083b0eacf67aa779e520
Julian Schmuckli <julian@schmuckli.net>
2019-04-06 16:26:28 +0200

    Added option to update password

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -2,6 +2,7 @@
   <v-container>
     <v-btn @click="back" flat><v-icon>keyboard_arrow_left</v-icon> back</v-btn>
     <h1>Settings</h1>
+    <h3>Profile</h3>
     <v-form ref="settings" @submit="save($event)">
       <v-layout row wrap>
         <v-flex xs12 sm6 md3>
@@ -21,6 +22,27 @@
       </v-layout>
       <v-btn type="submit">Save</v-btn>
     </v-form>
+    <h3>Update password</h3>
+    <v-form ref="password" @submit="updatePassword($event)">
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md3>
+          <v-text-field outline
+            label="New password"
+            type="password"
+            v-model="newPassword"
+          ></v-text-field>
+        </v-flex>
+        <v-flex xs12 sm6 md3>
+          <v-text-field
+            label="Repeat new password"
+            type="password"
+            outline
+            v-model="newPasswordRepeat"
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
+      <v-btn type="submit">Set password</v-btn>
+    </v-form>
   </v-container>
 </template>
 
@@ -33,7 +55,9 @@ export default {
   data(){
     return {
       email: firebase.auth().currentUser.email,
-      displayName: firebase.auth().currentUser.displayName
+      displayName: firebase.auth().currentUser.displayName,
+      newPassword: "",
+      newPasswordRepeat: ""
     }
   },
   methods: {
@@ -52,6 +76,21 @@ export default {
       }).catch(function(error) {
         StoreMod.showNotification("Error while saving profile: " + error);
       });
+    },
+    updatePassword(event){
+      event.preventDefault();
+
+      var user = firebase.auth().currentUser;
+
+      if(this.newPassword === this.newPasswordRepeat){
+        user.updatePassword(this.newPassword).then(function() {
+          StoreMod.showNotification("The password has been set.");
+        }).catch(function(error) {
+          StoreMod.showNotification(error);
+        });
+      } else {
+        StoreMod.showNotification("The passwords does not match.");
+      }
     }
   }
 }
@@ -60,5 +99,9 @@ export default {
 <style>
 div.v-text-field {
   margin: 10px !important;
+}
+
+h3{
+  margin-top:30px;
 }
 </style>
