9aa7d85238165423fc0eb839be6617193e5195f5
Julian Schmuckli <julian@schmuckli.net>
2019-04-25 23:12:11 +0200

    Added fade transition for router view

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,12 @@
     </v-toolbar>
 
     <v-content>
-      <router-view/>
+      <transition
+        name="fade"
+        mode="out-in"
+      >
+        <router-view/>
+      </transition>
       <footer>
         <p>{{ $t("general.thisIsAProjectBy") }}
           <a href="https://www.schmuckli.net/" target="_blank">Julian Schmuckli</a> |
@@ -143,5 +148,17 @@ footer a{
 
 h1{
   margin-bottom: 20px;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0
 }
 </style>
